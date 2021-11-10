df2628930f8b06be7178aa66763f079c861fdd38
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-11-09 17:40:31 -0800

    changes

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@
 const express = require('express');
 const { ApolloServer } = require( 'apollo-server-express' );
 const path = require('path');
-
+const mongoose = require("mongoose");
 const { authMiddleware } = require( './utils/auth' );
 const db = require('./config/connection');
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Book-Search',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
